9359f7295e8ea4f758b89862e71244142c34e91f
angelmtenor <angelmtenor@gmail.com>
2023-06-14 10:56:40 +0000

    feat:  Add dev and test environments

---
+++ {{ cookiecutter.__package_slug }}/.devcontainer/setup_user.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+# Author: Angel Martinez Tenor 2023
+
+# First setup required with new users in cloud / on prem machine (alredy done in dev containers)
+
+# USER LEVEL INSTRUCTIONS (NEVER USE SUDO):
+# 1: Enable execution:   `chmod +x setup_user.sh`
+# 2: Execute with comand `source setup_user.sh`
+
+
+# INSTALL MINICONDA (user)
+mkdir -p ~/miniconda3 && \
+  curl -sSL https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -o ~/miniconda3/miniconda.sh && \
+  bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3 && \
+  rm ~/miniconda3/miniconda.sh && \
+  ~/miniconda3/bin/conda init bash && \
+  ~/miniconda3/bin/conda init zsh
+source ~/.bashrc
+
+# Poetry
+curl -sSL https://install.python-poetry.org | python3 -
+# ENV PATH="$HOME/.local/bin:$PATH"
+echo 'export PATH="/home/'$USER'/.local/bin:$PATH"' >> ~/.bashrc
+
+# Git: Change default branch to main
+git config --global init.defaultBranch main
+source ~/.bashrc
+
+# Create custom DEV and TEST environments
+conda update --all -y
+
+# Dev enviornment with cookiecutter and ipykernel. Can be used with poetry, pip...
+conda create -n dev python=3.10 -y && conda init bash && \
+  echo "conda activate dev" >> ~/.bashrc && source activate dev && \
+  conda install -y -c conda-forge 'cookiecutter>=2' ipykernel
+echo "Environment dev created"
+
+# Test environmet. Dast evaluation with pip packages (no need for poetry)
+conda create -n test python=3.10 -y && conda activate test && \
+  pip install jupyter pandas transformers
+echo "Environment test created"
