75247c5b0e7fbae99b01c19f6072bdf6389553e2
agmn <angel.martinez-tenor@gft.com>
2023-09-25 16:13:00 +0200

    clean:  Unify dev containers (gpu as optional)

--- {{ cookiecutter.__package_slug }}/.devcontainer/setup_sudo.sh
+++ {{ cookiecutter.__package_slug }}/.devcontainer/setup_sudo.sh
@@ -2,3 +2,8 @@ sudo apt-get update --fix-missing -y && sudo apt-get upgrade -y \
   && sudo apt-get install -y --no-install-recommends git make curl wget bzip2 ca-certificates libglib2.0-0 libxext6 \
   libsm6 libxrender1 mercurial subversion nano htop gcc gpp clang linux-libc-dev
 
+# GPU-ONLY
+sudo apt-get install nvtop ubuntu-drivers-common -y
+sudo apt-get install nvidia-kernel-common-535 -y
+sudo reboot
+sudo apt install nvidia-cuda-toolkit -y
