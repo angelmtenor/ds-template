f209662831e26c62b587f31dde8fbcf2e57e69e8
Angel Martinez-Tenor <angel.martineztenor@plexus.es>
2024-05-19 12:02:34 +0200

    feat: Include system example

--- {{ cookiecutter.__package_slug }}/src/{{ cookiecutter.__package_slug }}/{{ cookiecutter.__package_slug }}.py
+++ {{ cookiecutter.__package_slug }}/src/{{ cookiecutter.__package_slug }}/{{ cookiecutter.__package_slug }}.py
@@ -1,3 +1,7 @@
 """
-Blank src module for {{ cookiecutter.__package_slug }}
+Template src module for {{ cookiecutter.__package_slug }}
 """
+
+from {{ cookiecutter.__package_slug }} import logger
+
+log = logger.get_logger(__name__)
